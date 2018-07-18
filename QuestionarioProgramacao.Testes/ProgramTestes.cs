fd88a8a7c024a1833cd698ed3bbbe5c0381c5e5c
Fabio Henrique Jugler <fabio@FABIOJUGLER-PC>
2018-07-18 18:37:19 -0300

    Adicionada biblioteca de testes.

---
+++ QuestionarioProgramacao.Testes/ProgramTestes.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionarioProgramacao.Testes
+{
+    [TestFixture]
+    public class ProgramTestes
+    {
+        [Test]
+        public void DevoSomarDoisNumeros()
+        {
+            
+
+            //Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
