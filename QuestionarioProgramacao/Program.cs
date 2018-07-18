28ddf7fcdd760d23d0a87bd5686ac4b2d98cfeb3
Fabio Henrique Jugler <fabio@FABIOJUGLER-PC>
2018-07-18 15:48:10 -0300

    commit inicial do projeto

---
+++ QuestionarioProgramacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace QuestionarioProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maiorNumero = 0;
+            int[] numeros = new int[5];
+            numeros[0] = 10;
+            numeros[1] = 52;
+            numeros[2] = 67;
+            numeros[3] = 31;
+            numeros[4] = 19;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maiorNumero)
+                    maiorNumero = numeros[i];
+            }
+
+            Console.WriteLine(maiorNumero);
+
+            // Mantém o console aberto.
+            Console.ReadKey();
+        }
+    }
+}
